3142e593df9bf9fc8965ad92b62ad3877e5ad879
thr3a <thr3a@example.com>
2025-12-22 00:34:26 +0900

    add:

---
+++ docker-build.sh
@@ -0,0 +1,2 @@
+# docker build -f ./amd-strix-halo-toolboxes/toolboxes/Dockerfile.vulkan-amdvlk -t vulkan-amdvlk:1.0 ./amd-strix-halo-toolboxes/toolboxes
+docker build -f ./amd-strix-halo-toolboxes/toolboxes/Dockerfile.rocm-7.1.1 -t rocm-7.1.1:1.0 ./amd-strix-halo-toolboxes/toolboxes
