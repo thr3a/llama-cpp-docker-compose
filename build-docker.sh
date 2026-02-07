bcae29d647d0e89adb7c08622665f7f017760da0
thr3a <thr3a@example.com>
2026-02-08 02:14:08 +0900

    add:

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,5 @@
-docker build -f ./amd-strix-halo-toolboxes/toolboxes/Dockerfile.vulkan-amdvlk -t vulkan-amdvlk:1.0 ./amd-strix-halo-toolboxes/toolboxes
-# docker build -f ./amd-strix-halo-toolboxes/toolboxes/Dockerfile.rocm-7.1.1 -t rocm-7.1.1:1.0 ./amd-strix-halo-toolboxes/toolboxes
+# docker build --pull=never -f ./amd-strix-halo-toolboxes/toolboxes/Dockerfile.vulkan-amdvlk -t vulkan-amdvlk:1.0 ./amd-strix-halo-toolboxes/toolboxes
+DOCKER_BUILDKIT=1 docker build --pull=false -f ./amd-strix-halo-toolboxes/toolboxes/Dockerfile.vulkan-amdvlk -t vulkan-amdvlk:1.0 ./amd-strix-halo-toolboxes/toolboxes
+# DOCKER_BUILDKIT=1 docker build --pull=false -f ./amd-strix-halo-toolboxes/toolboxes/Dockerfile.vulkan-amdvlk -t vulkan-amdvlk:1.0 ./amd-strix-halo-toolboxes/toolboxes
+# docker build -f ./amd-strix-halo-toolboxes/toolboxes/Dockerfile.rocm-6.4.4 -t rocm-6.4.4:1.0 ./amd-strix-halo-toolboxes/toolboxes
+# docker build -f ./amd-strix-halo-toolboxes/toolboxes/Dockerfile.rocm-7.2 -t rocm-7.2:1.0 ./amd-strix-halo-toolboxes/toolboxes
